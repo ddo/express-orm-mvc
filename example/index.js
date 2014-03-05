49ee1180a5291706f13b25b108ea2204a7547759
Ddo <joeddo89@gmail.com>
2014-03-05 16:20:30 +0700

    update example

--- example/index.js
+++ example/index.js
@@ -1,5 +1,7 @@
-var mvc = require('../index');
-mvc(function(err, app, db) {
+var mvc = require('express-orm-mvc');
+mvc({
+    path: __dirname
+}, function(err, app, db) {
     if(err)
         console.log(err);
 });
